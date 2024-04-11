5b8e41214a9e6ae8ec99a5d09eacace15d31acb6
aasane kariuki <franciskariki053@gmail.com>
2024-04-11 12:02:33 +0300

    adding JSON

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let menubar = document.querySelector('#menubar')
+let navbar = document.querySelector('navbar navbar-expand-lg bg-body-tertiary')
+
+menubar.oneclick = () => {
+    menubar.classList.toggle('fa-xmark');
+    navbar navbar-expand-lg bg-body-tertiary.classList.toggle('active');
+}
